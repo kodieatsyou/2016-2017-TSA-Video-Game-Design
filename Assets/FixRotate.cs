9e190178935d00686d21e5972dbf59e701b6558b
Tanner May <tannerjmay@gmail.com>
2016-11-03 23:31:26 -0600

    New Assets, snow particle, snow shader, and terrain reshape

---
+++ Assets/FixRotate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FixRotate : MonoBehaviour
+{
+
+    Quaternion rotation;
+    void Awake()
+    {
+        rotation = transform.rotation;
+    }
+    void LateUpdate()
+    {
+        transform.rotation = rotation;
+    }
+}
